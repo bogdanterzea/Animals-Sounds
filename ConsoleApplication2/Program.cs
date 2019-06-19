f0d49f5fa48e630eb794750dcd00b27f9dfb3b8e
bogdanterzea <terzeabogdan2007@yahoo.com>
2019-06-20 00:00:39 +0300

    Solved problem with objects

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Farm
+    {
+        string animal;
+        string sound;
+        public Farm(string nume, string sunet) {
+            animal = nume;
+            sound = sunet;
+        }
+        static void Main(string[] args)
+        {
+            Farm element = new Farm("Puiul", "PIU");
+            Farm element1 = new Farm("Vaca", "MUU");
+            Farm element2 = new Farm("Oaia", "BEE");
+            Farm element3 = new Farm("Porcul", "GUIT");
+
+            Console.WriteLine(element.animal + " scoate sunetul " + element.sound);
+            Console.WriteLine(element1.animal + " scoate sunetul " + element1.sound);
+            Console.WriteLine(element2.animal + " scoate sunetul " + element2.sound);
+            Console.WriteLine(element3.animal + " scoate sunetul " + element3.sound);
+
+            Console.ReadKey();
+        }
+    }
+}
